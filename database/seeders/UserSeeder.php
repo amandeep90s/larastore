71f066eb0a0fe91dd535292f7a16a13a8a81d9c0
Amandeep Singh <programmeramandeep@gmail.com>
2024-12-20 20:56:13 +0530

    feat: Define roles and permission with laravel spatie package

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\RolesEnum;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    User::factory()->create([
+      'name' => 'John Wick',
+      'email' => 'user@example.com',
+    ])->assignRole(RolesEnum::User->value);
+
+    User::factory()->create([
+      'name' => 'Mike Aurthur',
+      'email' => 'vendor@example.com',
+    ])->assignRole(RolesEnum::Vendor->value);
+
+    User::factory()->create([
+      'name' => 'Brett Lee',
+      'email' => 'admin@example.com',
+    ])->assignRole(RolesEnum::Admin->value);
+  }
+}
